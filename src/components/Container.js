4db43a67b2e1c83aa8608841196e024708eba979
Do Tuan Dat <75316700+dathymax@users.noreply.github.com>
2022-06-23 08:59:06 +0700

    update

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,39 +1,35 @@
-import Profile from './Profile'
+import Profile from "./Profile";
 import Posts from "./Home";
-import About from './About';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-  } from "react-router-dom";
-import Contact from './Contact';
-import PostInfo from './PostInfo';
+import About from "./About";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Contact from "./Contact";
+import PostInfo from "./PostInfo";
 
 const Container = () => {
-    return (
-        <div className="container">
-            <div className="content">
-                <Profile></Profile>
-                    
-                <div>
-                    <Switch>
-                        <Route  exact path="/">
-                            <Posts/>
-                        </Route>
-                        <Route path="/about">
-                            <About />
-                        </Route>
-                        <Route path="/contact">
-                            <Contact />
-                        </Route>
-                        <Route path="/postinfo/:id">
-                            <PostInfo />
-                        </Route>
-                </Switch>
-                </div>
-            </div>
-        </div>
-    )
-}
+	return (
+		<div className="container">
+			<div className="content">
+				<Profile></Profile>
 
-export default Container
+				<div>
+					<Switch>
+						<Route exact path="/miniblogs">
+							<Posts />
+						</Route>
+						<Route path="/about">
+							<About />
+						</Route>
+						<Route path="/contact">
+							<Contact />
+						</Route>
+						<Route path="/postinfo/:id">
+							<PostInfo />
+						</Route>
+					</Switch>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Container;
